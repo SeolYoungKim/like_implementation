22d2941df9a6c0459e35bd7f0f6535f5ed6a25f1
SeolYoungKim <nasur4da@gmail.com>
2023-02-06 21:40:48 +0900

    feat: project setting

---
+++ src/test/java/com/kimsy/community_service/CommunityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimsy.community_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

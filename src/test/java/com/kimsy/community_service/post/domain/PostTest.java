464b3c37270e23d4a5ff6b2953425aef959ca284
SeolYoungKim <nasur4da@gmail.com>
2023-02-07 20:22:49 +0900

    test: Post 수정 및 저자 검증 로직에 대한 테스트 코드 작성

--- src/test/java/com/kimsy/community_service/post/domain/PostTest.java
+++ src/test/java/com/kimsy/community_service/post/domain/PostTest.java
@@ -1,8 +1,10 @@
 package com.kimsy.community_service.post.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import com.kimsy.community_service.member.domain.AccountType;
 import com.kimsy.community_service.member.domain.Member;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -10,11 +12,11 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class PostTest {
+    private static final Member member = new Member("nickName", AccountType.LESSEE, 512L);
+
     @DisplayName("객체를 생성할 때")
     @Nested
     class Construct {
-        private final Member member = new Member("nickName", AccountType.LESSEE, 512L);
-
         @DisplayName("올바른 값이 넘어오는 경우 예외가 발생하지 않는다.")
         @Test
         void success() {
@@ -25,9 +27,56 @@ class PostTest {
         @ParameterizedTest
         @CsvSource({",contents", "title,", ",", "'',''", "'',contents", "title,''"})
         void fail(String title, String contents) {
-            Assertions.assertThatThrownBy(() -> new Post(title, contents, member))
+            assertThatThrownBy(() -> new Post(title, contents, member))
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessageContaining("제목이나 내용은 빈 값일 수 없습니다.");
+        }
+    }
+
+    @DisplayName("객체를 수정할 때")
+    @Nested
+    class Update {
+        private final Post post = new Post("title", "contents", member);
+
+        @DisplayName("올바른 값이 넘어오는 경우 예외가 발생하지 않고, Post의 제목과 내용이 수정된다.")
+        @Test
+        void success() {
+            final String expectedTitle = "제목";
+            final String expectedContents = "내용";
+            post.update(expectedTitle, expectedContents);
+
+            assertThat(post.getTitle()).isEqualTo(expectedTitle);
+            assertThat(post.getContents()).isEqualTo(expectedContents);
+        }
+
+        @DisplayName("제목이나 내용이 넘어오지 않는 경우 예외가 발생한다.")
+        @ParameterizedTest
+        @CsvSource({",contents", "title,", ",", "'',''", "'',contents", "title,''"})
+        void fail(String title, String contents) {
+            assertThatThrownBy(() -> post.update(title, contents))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("제목이나 내용은 빈 값일 수 없습니다.");
+        }
+    }
+
+    @DisplayName("게시글의 저자인지 확인할 때")
+    @Nested
+    class ValidateAuthor {
+        private final Post post = new Post("title", "contents", member);
+
+        @DisplayName("게시글의 저자일 경우 예외가 발생하지 않는다.")
+        @Test
+        void success() {
+            post.validateAuthor(member);
+        }
+
+        @DisplayName("게시글의 저자가 아닐 경우 예외를 발생시킨다.")
+        @Test
+        void fail() {
+            final Member notAuthor = new Member("임대인", AccountType.LESSOR, 34L);
+            assertThatThrownBy(() -> post.validateAuthor(notAuthor))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("게시글 작성자가 아니면 수정/삭제 요청을 할 수 없습니다.");
         }
     }
 }
